4423aab46d2fcf34c0774dc90986ebc004d060da
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-18 04:33:13 +0000

    Mar 17, 2024, 9:33 PM

--- helpers.cpp
+++ helpers.cpp
@@ -1,16 +1,16 @@
-
+#include "helpers.h"
 
 void readRegions(vector<Region>& regions)
 {
-  std::ifstream file("ZILLOW_REGIONS.csv");
-  std::string line;
-  while (std::getline(file, line)) {
-    std::stringstream ss(line);
-    std::string id, city, state;
-    std::getline(ss, id, ',');
-    std::getline(ss, city, ',');
-    std::getline(ss, state, ',');
-    regions.push_back({std::stoi(id), city, state});
+  ifstream file("ZILLOW_REGIONS.csv");
+  string line;
+  while (getline(file, line)) {
+    stringstream ss(line);
+    string id, city, state;
+    getline(ss, id, ',');
+    getline(ss, city, ',');
+    getline(ss, state, ',');
+    regions.push_back({stoi(id), city, state});
   }
   file.close();
 };
