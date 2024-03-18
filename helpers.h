d6848dd429905bde2d56be6c30f0e72a5e185efa
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-18 04:29:42 +0000

    Mar 17, 2024, 9:29 PM

--- helpers.h
+++ helpers.h
@@ -1,5 +1,15 @@
+
+using namespace std;
+#include <string>
+#include <vector>
 struct Region {
   int id;
-  string city;
-  string state;
+  std::string city;
+  std::string state;
+
+  bool operator<(const Region& other) const
+  {
+    return id < other.id;
+  }
 };
+void readRegions(vector<Region>& regions);
