298f657b61fcf69358961637d91b9e7b6f9e6f6b
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-18 04:40:15 +0000

    Mar 17, 2024, 9:40 PM

--- helpers.cpp
+++ helpers.cpp
@@ -2,15 +2,39 @@
 
 void readRegions(vector<Region>& regions)
 {
-  ifstream file("ZILLOW_REGIONS.csv");
+  vector<Region> regions;
+  ifstream file(filename);
   string line;
-  while (getline(file, line)) {
-    stringstream ss(line);
-    string id, city, state;
-    getline(ss, id, ',');
-    getline(ss, city, ',');
-    getline(ss, state, ',');
-    regions.push_back({stoi(id), city, state});
+
+  if (!file.is_open()) {
+    cerr << "Unable to open file: " << filename << endl;
+    return regions; // Return an empty vector if the file cannot be opened
   }
+
+  while (getline(file, line)) {
+    istringstream sstream(line);
+    string field;
+    Region region;
+
+    // Parse the ID
+    if (getline(sstream, field, ',')) {
+      region.id = stoi(field);
+    }
+
+    // Parse the City
+    if (getline(sstream, field, ',')) {
+      region.city = field;
+    }
+
+    // Parse the State
+    if (getline(sstream, field)) {
+      region.state = field;
+    }
+
+    // Add the Region to the vector
+    regions.push_back(region);
+  }
+
   file.close();
-};
+  return regions;
+}
